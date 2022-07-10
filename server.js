a3bb36bd34e5193f455552d11f5bfd0e3c6637a2
shaurya127 <shaurya1272000@gmail.com>
2022-07-10 17:55:40 +0530

    server code done

---
+++ server.js
@@ -0,0 +1,43 @@
+
+const admin = require("firebase-admin");
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 8080
+app.use(cors());
+var serviceAccount = require("./mindful-audio-337108-firebase-adminsdk-z6vij-072b95721d.json");
+app.use(express.json())
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+app.post('/alert',(req,res)=>{
+    console.log(req.body)
+    const message ={
+        notification:{
+            title:'New message',
+            body:'You have recieved new message'
+        },
+        token: req.body.tokan
+    }
+    admin.messaging().send(message).then((t)=>{
+        console.log('send sucess')
+    }).catch(err=>{
+        console.log(err)
+    })
+    res.send({
+        'note':'sucess alert'
+    })
+    
+})
+
+
+app.listen(port,()=>{
+    console.log('server running 8080')
+})
+
+
+        
